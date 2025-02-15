b82ef3441bf8356003fd55fafc63b844c7edc102
techatlasdev <srgoqui@gmail.com>
2025-02-14 22:42:37 -0500

    Mejorando Her

--- frontend/src/app/(landing)/@hero/page.tsx
+++ frontend/src/app/(landing)/@hero/page.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
       <h2 className="text-center bg-gradient-to-br from-white to-zinc-500 bg-clip-text text-5xl/[1.07] font-bold tracking-tight text-transparent md:text-7xl/[1.07]">A Smarter Way to Diagnose and Innovate in Healthcare</h2>
       <p className="text-center text-gray-400">Get instant medical insights, explore AI-driven diagnostics, and seamlessly connect with specialists—all in one platform.</p>
       <ButtonPrimary href="/auth">
-        Comenzar
+        Comenzar!
       </ButtonPrimary>
     </section>
   )
