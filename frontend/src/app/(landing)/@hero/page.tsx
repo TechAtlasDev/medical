2b4ec674dcac153ed488d799d41d3397d3d1b192
techatlasdev <srgoqui@gmail.com>
2025-02-14 22:16:16 -0500

    Solucionando redirecciones

--- frontend/src/app/(landing)/@hero/page.tsx
+++ frontend/src/app/(landing)/@hero/page.tsx
@@ -5,7 +5,9 @@ export default function Hero() {
     <section className="flex flex-col items-center gap-5 mt-32">
       <h2 className="text-center bg-gradient-to-br from-white to-zinc-500 bg-clip-text text-5xl/[1.07] font-bold tracking-tight text-transparent md:text-7xl/[1.07]">A Smarter Way to Diagnose and Innovate in Healthcare</h2>
       <p className="text-center text-gray-400">Get instant medical insights, explore AI-driven diagnostics, and seamlessly connect with specialists—all in one platform.</p>
-      <ButtonPrimary />
+      <ButtonPrimary href="/auth">
+        Comenzar
+      </ButtonPrimary>
     </section>
   )
 }
